10bfd454170dc25fd467174a8c013436db1cfa13
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-02-06 12:31:54 +0000

    customer service

--- CCakes/obj/Debug/net8.0/CCakes.AssemblyInfo.cs
+++ CCakes/obj/Debug/net8.0/CCakes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CCakes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09a849f195f967d371ba536953658a2ce5c94468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eeea23f2587f0df1daf937193b6f5619bb5e8f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CCakes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CCakes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
