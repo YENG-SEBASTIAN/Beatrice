6160635e0517b08cef4c25d91fb28507a401c55d
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-02-16 01:55:09 +0000

    repos

--- aspCake/obj/Debug/net8.0/aspCake.AssemblyInfo.cs
+++ aspCake/obj/Debug/net8.0/aspCake.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspCake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f14231fc452d178dcc33be64a87f15d24fca0a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+737aa640618b3f99210bb80e35e3d21c8dcb6e3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspCake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspCake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
