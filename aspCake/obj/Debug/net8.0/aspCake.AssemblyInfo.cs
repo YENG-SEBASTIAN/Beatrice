737aa640618b3f99210bb80e35e3d21c8dcb6e3f
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-02-16 01:43:08 +0000

    DB context

--- aspCake/obj/Debug/net8.0/aspCake.AssemblyInfo.cs
+++ aspCake/obj/Debug/net8.0/aspCake.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspCake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72c4d5c8711a5ddf35551e6a7a5733ecb020df7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f14231fc452d178dcc33be64a87f15d24fca0a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspCake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspCake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
