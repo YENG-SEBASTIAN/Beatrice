55f6e60f8b865315358bdf9a418bcd64f660bfcb
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-02-19 02:54:39 +0000

    customer post implemented

--- aspCake/Controllers/CustomerController.cs
+++ aspCake/Controllers/CustomerController.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using aspCake.Data.DTOs;
+using aspCake.Data.Service.Customer;
 
 namespace aspCake.Controllers
 {
@@ -10,6 +10,29 @@ namespace aspCake.Controllers
     [Route("api/[controller]")]
     public class CustomerController : ControllerBase
     {
-        
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost("customer")]
+        public async Task<IActionResult> AddCustomer([FromBody] CustomerDTO customerDTO)
+        {
+            try
+            {
+                bool result = await _customerService.InsertData(customerDTO);
+                if (result)
+                {
+                    return Ok();
+                }
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
